64a7fa12d514776adbfe010c64d3255e3037c02e
Lance Goodridge <ldgoodridge95@gmail.com>
2014-03-29 02:28:02 -0400

    Binded keypress to the actual inputBox itself instead of the entire page

--- views/js/user_input.js
+++ views/js/user_input.js
@@ -1,14 +1,13 @@
-// Set focus on inputBox
 $(document).ready(function() {
 	$("#inputBox").focus();
-});
 
-// Call sendToStory when ENTER is pressed
-$(document).keypress(function(event){
-	var key = event.keyCode || event.which;
-	if (key == 13) {
-		sendToStory();
-	}
+	// Sends to story when ENTER button is clicked
+	$("#inputBox").keypress(function(event){
+		var key = event.keyCode || event.which;
+		if (key == 13) {
+			sendToStory();
+		}
+	});
 });
 
 // Appends inputBox text to story paragraph
