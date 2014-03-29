24b5ed7cc36bb07e5b16820ff37b3b64d5cec9ff
Lisha Ruan <lisha.ruan@gmail.com>
2014-03-28 23:53:51 -0400

    Changed models

--- models.js
+++ models.js
@@ -1,6 +1,4 @@
 exports = module.exports = function(app, mongoose) {
 	//general sub docs
-	require('./schema/Assets')(app, mongoose);	
-	require('./schema/User')(app, mongoose);
-	require('./schema/Cache')(app, mongoose);
+	require('./schema/Story')(app, mongoose);
 }
