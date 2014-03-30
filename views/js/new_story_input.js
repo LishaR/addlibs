d167a4e807b715b70834d0e04a27c1b8ba1a626a
Lisha Ruan <lisha.ruan@gmail.com>
2014-03-30 00:48:29 -0400

    Changed button colors

--- views/js/new_story_input.js
+++ views/js/new_story_input.js
@@ -35,48 +35,55 @@ $(document).ready(function() {
 	});
 
 
-	//button mouse-over
-
 	$("#sendButton").mouseenter(function() {
+		console.log("enter");
 		$(this).css("background-color","#1C6B04");
 	});
 
 	$("#sendButton").mouseout(function() {
+		console.log("exit");
 		$(this).css("background-color","#55ff22");
 	});
 
 	$("#archiveButton").mouseenter(function() {
-		$(this).css("background-color","#dd1c00");
+		console.log("enter");
+		$(this).css("background-color","#1C6B04");
 	});
 
 	$("#archiveButton").mouseout(function() {
-		$(this).css("background-color","#ff3311");
+		console.log("exit");
+		$(this).css("background-color","#55ff22");
 	});
 
 	$("#newStory").mouseenter(function() {
-		$(this).css("background-color","#0077aa");
+		console.log("enter");
+		$(this).css("background-color","#015774");
 	});
 
 	$("#newStory").mouseout(function() {
-		$(this).css("background-color","#099cc");
+		console.log("exit");
+		$(this).css("background-color","#0099cc");
 	});
 
 	$("#homeButton").mouseenter(function() {
-		$(this).css("background-color","#111111");
+		console.log("enter");
+		$(this).css("background-color","#015774");
 	})
 
 	$("#homeButton").mouseout(function() {
-		$(this).css("background-color","#222222");
+		console.log("exit");
+		$(this).css("background-color","#000000");
 	})
 
 	// Hides the submit button and show character counter by default
 	hideSubmitButton();
+
 });
 
 
 // Appends inputBox text to story paragraph
-function sendToStory() {
-	$.get( "/updateStory" + '?part=' + $("#inputBox").val(), null, function(data) {	
+function createStory() {
+	$.get( "/createStory" + '?title=' + $("#titleBox").val() + '&part=' + $("#inputBox").val(), null, function(data) {	
 		console.log("call function");
 		window.location.href = "/viewStory";
 	});
@@ -121,13 +128,15 @@ function showSubmitButton() {
 	$("#counter").hide();
 }
 
- // // Shows the submit button and hides the character counter
- // function showSubmitButton() {
- // 	$("#sendButton").show();
- // 	$("#counter").hide();
- // }
+ // Shows the submit button and hides the character counter
+ function showSubmitButton() {
+ 	$("#sendButton").show();
+ 	$("#counter").hide();
+ }
 
- // // Hides the submit button and shows the character counter
- // function hideSubmitButton() {
- // 	$("#sendButton").hide();
- // 	$("#counter").show();
+ // Hides the submit button and shows the character counter
+ function hideSubmitButton() {
+ 	$("#sendButton").hide();
+ 	$("#counter").show();
+ }
+
