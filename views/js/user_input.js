bd7794f5e011627e8aa5ea4970887c7520fa6506
David Gilhooley <djg5@princeton.edu>
2014-03-30 00:30:48 -0400

    made the cursors change over all buttons

--- views/js/user_input.js
+++ views/js/user_input.js
@@ -60,11 +60,11 @@ $(document).ready(function() {
 	});
 
 	$("#homeButton").mouseenter(function() {
-		$(this).css("background-color","#015774");
+		$(this).css("background-color","rgb(41, 40, 40)");
 	})
 
 	$("#homeButton").mouseout(function() {
-		$(this).css("background-color","#000000");
+		$(this).css("background-color","rgb(109, 108, 108)");
 	})
 
 	// Hides the submit button and show character counter by default
